eeec4021e30c421e5f057cf8df602117f6e7780c
pau <santosvalla@gmail.com>
2023-09-22 11:52:52 +0200

    nuevos ejercicios

--- app/src/main/java/com/example/tema2actividades/Actividad16.kt
+++ app/src/main/java/com/example/tema2actividades/Actividad16.kt
@@ -1,5 +1,26 @@
 package com.example.tema2actividades
 
+import java.util.Scanner
+
 fun main() {
-    //
+    //16. Ingresar 10 valores enteros por teclado. Contar cuántos de dichos valores ingresados
+    //fueron 0, y cuántos de valores diferentes a 0. Recuerda utilizar la cláusula when.
+    val sc = Scanner(System.`in`)
+
+    var valor:Int
+    var ceros = 0
+    var noCeros = 0
+    for (i in 1 until 11) {
+        print("Introduce el valor ${i}: ")
+        valor = sc.nextInt()
+        when (valor) {
+            0 -> ceros++
+            else -> noCeros++
+        }
+    }
+
+    println("Valores iguales a cero: $ceros")
+    println("Valores diferentes a cero: $noCeros")
+
+
 }
